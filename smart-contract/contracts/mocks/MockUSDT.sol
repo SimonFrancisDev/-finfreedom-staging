adc2462a040cbaba4118fc4806cdf2019cf493bd
SimonFrancisDev <francischukwuemeka10@gmail.com>
2026-07-04 20:46:34 +0100

    Reset fresh staging deployment

--- smart-contract/contracts/mocks/MockUSDT.sol
+++ smart-contract/contracts/mocks/MockUSDT.sol
@@ -6,27 +6,38 @@ contract MockUSDT {
     string public name = "USDT";
     string public symbol = "USDT";
     uint8 public decimals = 6;
+    uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
     function mint(address to, uint256 amount) external {
+        require(to != address(0), "mint to zero");
+        totalSupply += amount;
         balanceOf[to] += amount;
+        emit Transfer(address(0), to, amount);
     }
 
     function approve(address spender, uint256 amount) external returns (bool) {
         allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
         return true;
     }
 
     function transfer(address to, uint256 amount) external returns (bool) {
+        require(to != address(0), "transfer to zero");
         require(balanceOf[msg.sender] >= amount, "no balance");
         balanceOf[msg.sender] -= amount;
         balanceOf[to] += amount;
+        emit Transfer(msg.sender, to, amount);
         return true;
     }
 
     function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        require(to != address(0), "transfer to zero");
         require(balanceOf[from] >= amount, "no balance");
         require(allowance[from][msg.sender] >= amount, "no allowance");
 
@@ -34,6 +45,8 @@ contract MockUSDT {
         balanceOf[from] -= amount;
         balanceOf[to] += amount;
 
+        emit Approval(from, msg.sender, allowance[from][msg.sender]);
+        emit Transfer(from, to, amount);
         return true;
     }
 }
